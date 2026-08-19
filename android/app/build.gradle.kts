e0f0e7b1db89baeda9455196e10e46d005493bf4
MurhafMoussa <murhaf.moussa@hotmail.com>
2026-08-19 15:28:23 +0300

    chore: update launch configurations and platform build settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.dndn_platform_test"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
